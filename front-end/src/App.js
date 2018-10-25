94df576b5c273308d693c81c177cfaf44efbb3aa
Eduard Nikolaiev <the.aloner@gmail.com>
2018-10-26 01:27:01 +0300

    Added tabulator with mock data, menu, reset.scss

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,34 +1,27 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import PreLoader from './components/PreLoader';
-import {ReactTabulator} from 'react-tabulator';
-import 'react-tabulator/lib/styles.css';
-import Tabulator from 'tabulator-tables';
-import 'tabulator-tables/dist/css/tabulator.min.css';
+import './Reset.scss';
 import './App.scss';
-
-const columns = [
-  { title: 'Name', field: 'name', width: 150 },
-  { title: 'BirthDate', field: 'birthDate', align: 'left' },
-  { title: 'PhoneNumber', field: 'phoneNumber' },
-  { title: 'CardId', field: 'cardId', align: 'center' }
-];
-
-const data = [
-  { id: 1, name: 'Dmitriy Novikov', birthDate: '18.07.1987', phoneNumber: '+380956358965', cardId: '265987456' },
-  { id: 2, name: 'Alexey Fateev', birthDate: '13.04.1984', phoneNumber: '+380638954782', cardId: '852410003' },
-  { id: 3, name: 'Serhii Vakulenko', birthDate: '01.01.1999', phoneNumber: '+380508961200', cardId: '596001478' },
-  { id: 4, name: 'Serhii Harmash', birthDate: '12.02.1998', phoneNumber: '+380448907685', cardId: '768900112' }
-];
+import Home from './components/Home';
+import Menu from './components/Menu';
+import Packages from './components/Packages';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
   render () {
     return (
-      <Fragment>
-        <div className="App-preloader" ref="preloader"><PreLoader fullScreen={false}/></div>
+      <Router>
         <div className="App">
-          <ReactTabulator />
+          <div className="App-preloader" ref="preloader"><PreLoader fullScreen={false}/></div>
+          <div className="App__leftpanel">
+            <Menu />
+          </div>
+          <div className="App__rightpanel">
+            <Route exact path="/" component={Home} />
+            <Route path="/packages" component={Packages} />
+          </div>
         </div>
-      </Fragment>
+      </Router>
     );
   }
 
