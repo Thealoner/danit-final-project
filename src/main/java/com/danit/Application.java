2623cfaefae89b0d75750af68bd27bec60fa461f
Daria <darrr2013@gmail.com>
2018-10-26 12:27:19 +0300

    jwt authentication

--- src/main/java/com/danit/Application.java
+++ src/main/java/com/danit/Application.java
@@ -1,13 +1,30 @@
 package com.danit;
 
+import com.danit.model.ApplicationUser;
+import com.danit.repository.ApplicationUserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @ComponentScan("com.danit")
 @SpringBootApplication
 public class Application {
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/danit/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.danit.controllers;
+
+import com.danit.model.ApplicationUser;
+import com.danit.repository.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+  private ApplicationUserRepository applicationUserRepository;
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+  public UserController(ApplicationUserRepository applicationUserRepository,
+                        BCryptPasswordEncoder bCryptPasswordEncoder) {
+    this.applicationUserRepository = applicationUserRepository;
+    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+  }
+
+  @PostMapping("/sign-up")
+  public void signUp(@RequestBody ApplicationUser user) {
+    user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+    applicationUserRepository.save(user);
+  }
+}
