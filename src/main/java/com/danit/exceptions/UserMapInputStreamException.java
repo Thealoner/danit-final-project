f07c9e28e67850fe2775988a94afa2668046fd48
Garmash.S <garmashs@gmail.com>
2018-10-31 15:56:01 +0200

    #custom user defined exception just for code style

--- src/main/java/com/danit/exceptions/UserMapInputStreamException.java
+++ src/main/java/com/danit/exceptions/UserMapInputStreamException.java
@@ -2,12 +2,12 @@ package com.danit.exceptions;
 
 public class UserMapInputStreamException extends RuntimeException {
 
-    public UserMapInputStreamException(String message) {
-      super(message);
-    }
+  public UserMapInputStreamException(String message) {
+    super(message);
+  }
 
-    public UserMapInputStreamException(String message, Throwable throwable) {
-      super(message, throwable);
-    }
+  public UserMapInputStreamException(String message, Throwable throwable) {
+    super(message, throwable);
+  }
 
 }
