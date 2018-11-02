a4453435db452fc682b89c2f06633e8d22406c41
Daria Lazurenko <darrr@users.noreply.github.com>
2018-11-02 18:43:48 +0200

    Update Application.java

--- src/main/java/com/danit/Application.java
+++ src/main/java/com/danit/Application.java
@@ -1,7 +1,5 @@
 package com.danit;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,5 +10,4 @@ public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
-
 }
