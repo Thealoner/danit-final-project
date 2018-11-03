09d6ba0eb61fa05970df2bc38e8c4c4827c216e4
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-11-03 15:16:22 +0200

    remove header, grid-layout

--- front-end/src/components/Manager/index.js
+++ front-end/src/components/Manager/index.js
@@ -1,13 +1,11 @@
 import React, {Component, Fragment} from 'react';
 import './index.scss';
 import Main from './Main';
-import Header from './Header';
 
 class Menu extends Component {
   render () {
     return (
       <Fragment>
-        <Header/>
         <Main/>
       </Fragment>
     );
