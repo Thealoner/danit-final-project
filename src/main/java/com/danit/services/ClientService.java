4f6e775cb69ac9e66b7d3b141061d3d11730bd48
Serhii Harmash <garmashs@gmail.com>
2018-11-03 12:28:51 +0200

    #removed unused imports again

--- src/main/java/com/danit/services/ClientService.java
+++ src/main/java/com/danit/services/ClientService.java
@@ -3,7 +3,6 @@ package com.danit.services;
 import com.danit.models.Client;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ClientService {
   List<Client> getAllClients();
--- src/main/java/com/danit/services/UserService.java
+++ src/main/java/com/danit/services/UserService.java
@@ -3,7 +3,6 @@ package com.danit.services;
 import com.danit.models.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserService {
 
