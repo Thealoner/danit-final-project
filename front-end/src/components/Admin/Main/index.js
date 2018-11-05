af69e0ea6bf2bf2473617db6dd7ba125658d0459
Oleg <olegmikadze10@gmail.com>
2018-11-05 18:39:50 +0200

    Tabs added + authentefication changed (#48)
    * tabs added + authentification changed
    
    * tabs
    
    * eslint changes added
    
    * eslint changes added
    
    * changing tabs doing
    
    * Merging continued
    
    * Fixed merge error (duplicate line) and added VSCode exceptions to .gitignore
    
    * Tabs switch with routing works now
    
    * Saving current state (content) for every tab
    
    * Almost there...
    
    * Some fixes
    
    * Proper saving tabs state when viewing a particular record/row
    
    * Dynamic generation for the Record component using GridEntities config
    
    * Fixed url for new tabs
    
    * eslint fixes

--- front-end/src/components/Admin/Main/index.js
+++ front-end/src/components/Admin/Main/index.js
@@ -1,42 +1,109 @@
 import React, { Component } from 'react';
-import { NavLink, Route } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './index.scss';
-import Grid from './Grid';
-import GridEntities from './GridEntities';
-import Record from './Record';
+import TabbedArea from './TabbedArea';
+import EntitiesMenu from './EntitiesMenu';
+
+let index = 1;
 
 class Main extends Component {
   state = {
-    links: []
+    tabs: [{
+      title: 'Title',
+      tabKey: '1',
+      contentUrl: ''
+    }],
+    activeKey: '1'
   };
 
-  constructor (props) {
-    super(props);
-
-    GridEntities.forEach((entity) => {
-      this.state.links.push(
-        <li key={entity.id} className="main__menuitem">
-          <NavLink exact to={'/admin/' + entity.id} activeClassName="menu__link--active" className="menu__link">{entity.name}</NavLink>
-        </li>
-      );
+  add = (e) => {
+    e.stopPropagation();
+    index++;
+    const newTab = {
+      title: `Title: ${index}`,
+      tabKey: `${index}`,
+      contentUrl: ''
+    };
+    
+    this.setState({
+      tabs: this.state.tabs.concat(newTab),
+      activeKey: `${index}`
     });
+    
+    this.props.history.push('/admin/' + index + '/');
+  };
+
+  onTabChange = (activeKey) => {
+    this.setState({
+      activeKey
+    });
+
+    let clickedTab = this.state.tabs.find((tab) => {
+      return tab.tabKey === activeKey;
+    });
+
+    this.props.history.push('/admin/' + activeKey + '/' + clickedTab.contentUrl);
+  };
+
+  remove = (tabKey, e) => {
+    e.stopPropagation();
+    if (this.state.tabs.length === 1) {
+      alert('Error. You cannot delete this tab');
+      return;
+    }
+    let foundIndex = 0;
+    const after = this.state.tabs.filter((t, i) => {
+      if (t.tabKey !== tabKey) {
+        return true;
+      }
+      foundIndex = i;
+      return false;
+    });
+
+    let activeKey = this.state.activeKey;
+    if (activeKey === tabKey) {
+      if (foundIndex) {
+        foundIndex--;
+      }
+      activeKey = after[foundIndex].tabKey;
+    }
+    this.setState({
+      tabs: after,
+      activeKey
+    });
+  };
+
+  setTabContentUrl = (url) => {
+    let currenTab = this.state.tabs.find((tab) => {
+      return tab.tabKey === this.state.activeKey;
+    });
+
+    currenTab.contentUrl = url;
   }
 
   render () {
     return (
+
       <main className="main">
         <div className="main__left">
           <ul className="main__menu">
-            {this.state.links}
+            <EntitiesMenu activeKey={this.state.activeKey} />
           </ul>
         </div>
         <div className="main__right">
-          <Route exact path="/admin/:entityType" component={Grid} />
-          <Route path="/admin/:entityType/:rowId" component={Record} />
+          <TabbedArea
+            className="tabs"
+            add={this.add}
+            onTabChange={this.onTabChange}
+            remove={this.remove}
+            activeKey={this.state.activeKey}
+            tabs={this.state.tabs}
+            setTabContentUrl={this.setTabContentUrl}
+          />
         </div>
       </main>
     );
   }
 }
 
-export default Main;
+export default withRouter(Main);
