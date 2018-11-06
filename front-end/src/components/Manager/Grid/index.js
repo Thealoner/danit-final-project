1f34f41951efee2b0d7ef0236cb22625c7cb9604
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-11-06 18:27:56 +0200

    bugs fixes

---
+++ front-end/src/components/Manager/Grid/index.js
@@ -0,0 +1,108 @@
+import React, { Component, Fragment } from 'react';
+import { ReactTabulator } from 'react-tabulator';
+import 'react-tabulator/lib/styles.css';
+import 'tabulator-tables/dist/css/tabulator.min.css';
+import axios from 'axios';
+import GridEntities from '../GridEntities';
+
+class Grid extends Component {
+  state = {
+    id: '',
+    name: '',
+    data: [],
+    columns: [
+      { title: 'ID', field: 'id', width: 150 },
+      { title: 'Номер карты', field: 'cardId' },
+      { title: 'Имя', field: 'firstName' },
+      { title: 'Фамилия', field: 'lastName', align: 'left' },
+      { title: 'Дата рождения', field: 'birthDate' },
+      { title: 'Пол', field: 'gender' },
+      { title: 'E-mail', field: 'email' },
+      { title: 'Номер телефона', field: 'phoneNumber' }
+    ]
+  };
+  ref = null;
+
+  rowClick = (e, row) => {
+    console.log('ref table: ', this.ref.table); // this is the Tabulator table instance
+    console.log('rowClick id:' + row.getData().id, row, e);
+  };
+
+  setData = () => {
+    axios.get('http://localhost:9000/api/clients/all')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        const data = response.data.slice(0, 100);
+        this.setState({
+          id: this.props.match.params.entityId,
+          data: data
+          // ,columns: entity.columns
+        });
+      }.bind(this))
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  };
+
+  setSampleData = () => {
+    let entityId = this.props.match.params.entityId;
+    
+    let entity = GridEntities.find((el) => {
+      return el.id === entityId;
+    });
+
+    this.setState({
+      id: entity.id,
+      data: entity.sampleData,
+      columns: entity.columns
+    });
+  };
+
+  clearData = () => {
+    this.setState({ data: [] });
+  };
+
+  render () {
+    const options = {
+      movableRows: true
+    };
+    
+    return (
+      <Fragment>
+        <ReactTabulator
+          ref={ref => (this.ref = ref)}
+          columns={this.state.columns}
+          data={this.state.data}
+          rowClick={this.rowClick}
+          options={options}
+          data-custom-attr="test-custom-attribute"
+          className="custom-css-class"
+        />
+        <h3>Asynchronous data: (e.g. fetch) -
+          <button onClick={this.setSampleData}>Set Sample Data</button>{' '}
+          <button onClick={this.setData}>Set Data From Server</button>{' '}
+          <button onClick={this.clearData}>Clear</button>
+        </h3>
+      </Fragment>
+    );
+  }
+
+  componentDidMount () {
+    this.setSampleData();
+  }
+
+  componentDidUpdate () {
+    let entityId = this.props.match.params.entityId;
+    
+    if (entityId !== this.state.id) {
+      this.setSampleData();
+    }
+  }
+}
+
+export default Grid;
