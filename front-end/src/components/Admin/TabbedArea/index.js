b83d34e5d73fa4d538289b5531bb7a49aae3c0d6
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-11-08 22:53:13 +0200

    styles for tabs

--- front-end/src/components/Admin/TabbedArea/index.js
+++ front-end/src/components/Admin/TabbedArea/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './index.scss';
 import Tabs, {TabPane} from 'rc-tabs';
 import 'rc-tabs/assets/index.css';
@@ -7,8 +7,19 @@ import ScrollableInkTabBar from 'rc-tabs/lib/ScrollableInkTabBar';
 import { Route, NavLink } from 'react-router-dom';
 import Grid from '../Grid';
 import Record from '../Record';
+import $ from 'jquery';
 
 class TabbedArea extends Component {
+  componentDidMount () {
+    $('.rc-tabs').addClass('tabs_area');
+    $('.rc-tabs-bar').addClass('tabs_area__header');
+    $('.rc-tabs-content').addClass('tabs_area__content');
+    $('.rc-tabs-tab:not(.rc-tabs-tab:last-child)').addClass('tabs_area__tab');
+    $('.rc-tabs-nav-container, .rc-tabs-nav-wrap, .rc-tabs-nav-scroll, .rc-tabs-nav').css('height', '100%');
+    $('.rc-tabs-nav').removeClass('rc-tabs-nav-animated');
+    $('.rc-tabs-ink-bar').removeClass('rc-tabs-ink-bar-animated');
+  }
+
   construct () {
     const disabled = true;
 
@@ -16,51 +27,33 @@ class TabbedArea extends Component {
       return (
         <TabPane
           key={t.tabKey}
-          tab = {<span>{t.title}
-            <NavLink
-              to={'/admin/' + t.tabKey}
-              className='tabbedarea__close'
-              onClick={this.props.remove.bind(this, t.tabKey)}
-            >x</NavLink>
-          </span>}
-        >
-          <div>
-            <Route exact path="/admin/:tabKey/:entityType" render={
-              (props) => <Grid setTabContentUrl={this.props.setTabContentUrl} {...props} />
-            } />
-            <Route path="/admin/:tabKey/:entityType/:rowId" render={
-              (props) => <Record setTabContentUrl={this.props.setTabContentUrl} {...props} />
-            } />
-          </div>
+          tab = {<span className="tabs_area__title-wrapper">{t.title}
+            <NavLink to={'/admin/' + t.tabKey} className='tabs_area__close'
+              onClick={this.props.remove.bind(this, t.tabKey)}>x</NavLink>
+          </span>}>
+          <Route exact path="/admin/:tabKey/:entityType" render={
+            (props) => <Grid setTabContentUrl={this.props.setTabContentUrl} {...props} />
+          } />
+          <Route path="/admin/:tabKey/:entityType/:rowId" render={
+            (props) => <Record setTabContentUrl={this.props.setTabContentUrl} {...props} />} />
         </TabPane>
       );
     }).concat([
-      <TabPane
-        key={'__add'}
-        disabled={disabled}
-        tab={
-          <span
-            className='tabbedarea__link'
-            onClick={this.props.add}>
+      <TabPane key={'__add'} disabled={disabled} tab={<span className='tabs_area__link' onClick={this.props.add}>
             + Add
-          </span>
-        }
-      />
+      </span>}/>
     ]);
   }
 
   render () {
     return (
-      <div>
-        <Tabs
-          renderTabBar={() => <ScrollableInkTabBar/>}
-          renderTabContent={() => <TabContent/>}
-          activeKey={this.props.activeKey}
-          onChange={this.props.onTabChange}
-        >
-          {this.construct()}
-        </Tabs>
-      </div>
+      <Tabs
+        renderTabBar={() => <ScrollableInkTabBar/>}
+        renderTabContent={() => <TabContent animated={false}/>}
+        activeKey={this.props.activeKey}
+        onChange={this.props.onTabChange}>
+        {this.construct()}
+      </Tabs>
     );
   }
 }
