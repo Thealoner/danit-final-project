f34524da077328ad09b2386e138a60fd62c423dc
Thealoner <the.aloner@gmail.com>
2018-11-08 12:43:47 +0200

    Load clients from server (instead of sampleData) (#59)
    * First try
    
    * Done for grids and simple records

--- front-end/src/components/Admin/Record/SimpleRecord/index.js
+++ front-end/src/components/Admin/Record/SimpleRecord/index.js
@@ -3,52 +3,77 @@ import './index.scss';
 import { ReactTabulator } from 'react-tabulator';
 import 'react-tabulator/lib/styles.css';
 import 'tabulator-tables/dist/css/tabulator.min.css';
-import GridEntities from '../../GridEntities';
+import { getEntityByType } from '../../GridEntities';
+import AuthService from '../../../Login/AuthService';
+import Settings from '../../../Settings';
 
 class SimpleRecord extends Component {
   state = {
-    id: '',
+    entityType: '',
     name: '',
+    data: [],
     columns: [
       { title: 'Key', field: 'key', width: 150 },
       { title: 'Value', field: 'value', align: 'left' }
     ]
   };
 
+  getData = () => {
+    let { rowId } = this.props.match.params;
+    let { entityType } = this.props;
+    let entity = getEntityByType(entityType);
+    let authService = new AuthService();
+
+    if (authService.loggedIn() && !authService.isTokenExpired()) {
+      const headers = {
+        'Content-Type': 'application/json'
+      };
+
+      let token = authService.getToken();
+      headers['Authorization'] = token;
+
+      fetch(
+        Settings.apiServerUrl + entity.apiUrl + '/' + rowId,
+        { headers }
+      )
+        .then(authService._checkStatus)
+        .then(response => response.json())
+        .then(data => {
+          let keys = Object.keys(data);
+          let dataArray = [];
+    
+          keys.forEach((key) => {
+            dataArray.push({
+              key: key,
+              value: data[key]
+            });
+          });
+
+          this.setState({
+            entityType: entityType,
+            data: dataArray
+            // ,columns: entity.columns
+          });
+        });
+    } else {
+      console.log('Not logged in or token is expired');
+    }
+  };
+
   render () {
+    let { rowId } = this.props.match.params;
+    let { entityType, setTabContentUrl } = this.props;
+    setTabContentUrl(entityType + '/' + rowId);
+
     const options = {
       movableRows: true
     };
-    
-    let { entityType, setTabContentUrl } = this.props;
-    let rowId = this.props.match.params.rowId;
-    
-    setTabContentUrl(entityType + '/' + rowId);
-
-    let entity = GridEntities.find((el) => {
-      return el.id === entityType;
-    });
-    
-    let dataRecord = entity.sampleData.find((el) => {
-      return el.id === +rowId;
-    });
-    
-    let keys = Object.keys(dataRecord);
-    let data = [];
-    
-    keys.forEach((key) => {
-      data.push({
-        key: key,
-        value: dataRecord[key]
-      });
-    });
 
     return (
-
       <ReactTabulator
         ref={ref => (this.ref = ref)}
         columns={this.state.columns}
-        data={data}
+        data={this.state.data}
         rowClick={this.rowClick}
         options={options}
         data-custom-attr="test-custom-attribute"
@@ -58,7 +83,7 @@ class SimpleRecord extends Component {
   }
 
   componentDidMount () {
-    
+    this.getData();
   }
 }
 
