8ebf64fed2fcf1d1aaff051b0cd8c9f930725bb0
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-11-09 17:07:08 +0200

    close-btn

--- front-end/src/components/Admin/TabbedArea/index.js
+++ front-end/src/components/Admin/TabbedArea/index.js
@@ -19,20 +19,18 @@ class TabbedArea extends Component {
     const disabled = true;
 
     return this.props.tabs.map((t) => {
-      return (
-        <TabPane
-          key={t.tabKey}
-          tab = {<span className="rc-tabs__title-wrapper">{t.title}
-            <NavLink to={'/admin/' + t.tabKey} className='rc-tabs__close'
-              onClick={this.props.remove.bind(this, t.tabKey)}>x</NavLink>
-          </span>}>
-          <Route exact path="/admin/:tabKey/:entityType" render={
-            (props) => <Grid setTabContentUrl={this.props.setTabContentUrl} {...props} />
-          } />
-          <Route path="/admin/:tabKey/:entityType/:rowId" render={
-            (props) => <Record setTabContentUrl={this.props.setTabContentUrl} {...props} />} />
-        </TabPane>
-      );
+      return <TabPane
+        key={t.tabKey}
+        tab={<span className="rc-tabs__title-wrapper">{t.title}
+          <NavLink to={'/admin/' + t.tabKey} className='rc-tabs__close-btn'
+            onClick={this.props.remove.bind(this, t.tabKey)}/>
+        </span>}>
+        <Route exact path="/admin/:tabKey/:entityType" render={
+          (props) => <Grid setTabContentUrl={this.props.setTabContentUrl} {...props} />
+        }/>
+        <Route path="/admin/:tabKey/:entityType/:rowId" render={
+          (props) => <Record setTabContentUrl={this.props.setTabContentUrl} {...props} />}/>
+      </TabPane>;
     }).concat([
       <TabPane key={'__add'} disabled={disabled} tab={<span className='rc-tabs__add-btn' onClick={this.props.add}/>}/>
     ]);
