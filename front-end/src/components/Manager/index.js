68d6713a523817b0e97d7662f1773c874bd38606
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-11-12 19:13:08 +0200

    jquery remove

--- front-end/src/components/Manager/index.js
+++ front-end/src/components/Manager/index.js
@@ -19,7 +19,7 @@ class Manager extends Component {
     e.stopPropagation();
     index++;
     const newTab = {
-      title: `Title: ${index}`,
+      title: 'Title',
       tabKey: `${index}`,
       contentUrl: ''
     };
@@ -72,19 +72,28 @@ class Manager extends Component {
     });
   };
 
-  setTabContentUrl = (url) => {
-    let currenTab = this.state.tabs.find((tab) => {
+  setTabTitle = (title) => {
+    let currentTab = this.state.tabs.find((tab) => {
       return tab.tabKey === this.state.activeKey;
     });
 
-    currenTab.contentUrl = url;
+    currentTab.title = title;
+    return currentTab.title;
+  };
+
+  setTabContentUrl = (url) => {
+    let currentTab = this.state.tabs.find((tab) => {
+      return tab.tabKey === this.state.activeKey;
+    });
+
+    currentTab.contentUrl = url;
   };
 
   render () {
     return (
       <main className="configurator">
         <div className="configurator__left">
-          <EntitiesMenu activeKey={this.state.activeKey} />
+          <EntitiesMenu activeKey={this.state.activeKey} setTabTitle={this.setTabTitle}/>
         </div>
         <div className="configurator__right">
           <TabbedArea
