8296acffc9441fbeddacae8bc2c257a1ded9fe49
Daria Lazurenko <darrr@users.noreply.github.com>
2018-11-16 21:07:53 +0200

    client-dto-starterpack (#82)
    * client-dto-starterpack
    
    * codesmell fix
    
    * codesmell fix 2

---
+++ src/main/java/com/danit/dto/clientdto/ClientDto.java
@@ -0,0 +1,34 @@
+package com.danit.dto.clientdto;
+
+import com.danit.utils.CustomDateAndTimeDeserialize;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import java.util.Date;
+
+public class ClientDto {
+
+  @Null
+  private Long id;
+
+  @NotNull
+  private String firstName;
+  @NotNull
+  private String lastName;
+  @NotNull
+  private String gender;
+
+  @JsonDeserialize(using = CustomDateAndTimeDeserialize.class)
+  @JsonFormat(
+      shape = JsonFormat.Shape.STRING,
+      pattern = "yyyy-MM-dd")
+  private Date birthDate;
+
+  @NotNull
+  private String phoneNumber;
+
+  @NotNull
+  private String email;
+}
