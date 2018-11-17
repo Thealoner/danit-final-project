82c5b0ac4a561791fdc30c014837059d8cc08bab
Serhii Harmash <garmashs@gmail.com>
2018-11-17 14:31:18 +0200

    #removed unused imports

--- src/main/java/com/danit/controllers/ClientController.java
+++ src/main/java/com/danit/controllers/ClientController.java
@@ -4,7 +4,6 @@ package com.danit.controllers;
 import com.danit.models.Client;
 import com.danit.services.ClientService;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
