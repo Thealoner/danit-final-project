c3ebd300502e289062f326b550115a55dfa91825
Daria <darrr2013@gmail.com>
2018-11-21 21:03:34 +0200

    buf fix and one test

--- src/main/java/com/danit/controllers/ClientController.java
+++ src/main/java/com/danit/controllers/ClientController.java
@@ -9,7 +9,6 @@ import com.danit.services.ClientService;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
