1f2f7573cadc847402f2bd174556f438b2adcfb6
karynas <44035593+karynas@users.noreply.github.com>
2018-11-24 12:18:06 +0200

    Add energym logo to the app's header (#93)

--- front-end/src/components/Header/index.js
+++ front-end/src/components/Header/index.js
@@ -2,16 +2,17 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import './index.scss';
 import User from './User';
+import logo from './EG-logo.svg';
 
 class Header extends Component {
   render () {
     const {handleLogout, userName} = this.props;
-    return (
-      <div className="header">
-        <Link to="/" className="header__logo">LOGO</Link>
-        <User handleLogout={handleLogout} userName={userName}/>
-      </div>
-    );
+    return <div className="header">
+      <Link to="/" className="header__logo-link">
+        <img className="header__logo" src={logo} alt="logo"/>
+      </Link>
+      <User handleLogout={handleLogout} userName={userName}/>
+    </div>;
   }
 }
 
