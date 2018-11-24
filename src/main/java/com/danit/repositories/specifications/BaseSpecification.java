a9494bf71d9d2fba6b3f4294abc10fe26c5f4927
Eduard Nikolaiev <the.aloner@gmail.com>
2018-11-24 19:54:18 +0200

    modificator order

--- src/main/java/com/danit/repositories/specifications/BaseSpecification.java
+++ src/main/java/com/danit/repositories/specifications/BaseSpecification.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.domain.Specification;
 
 public abstract class BaseSpecification<T, U> {
 
-  private final static String wildcard = "%";
+  private static final String wildcard = "%";
 
   public abstract Specification<T> getFilter(U request);
 
