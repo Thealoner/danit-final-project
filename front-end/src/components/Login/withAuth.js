73f77d57baa637966fde83ea29e4cee8a6f708cc
Thealoner <the.aloner@gmail.com>
2018-11-29 22:58:56 +0200

    Added eslint-plugin-react and fixed a few linted issues (#104)
    * Added eslint-plugin-react and fixed a few linted issues
    
    * eslint command in yml fix
    
    * added eslint plugin "import"
    
    * eslint dependency issue fix
    
    * eslint dependency issue fix

--- front-end/src/components/Login/withAuth.js
+++ front-end/src/components/Login/withAuth.js
@@ -15,7 +15,7 @@ export default function withAuth (AuthComponent) {
       };
     }
 
-    componentWillMount () {
+    UNSAFE_componentWillMount () {
       if (!Auth.loggedIn()) {
         this.props.history.replace('/login');
       } else {
