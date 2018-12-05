7b1c4bd6368e93ab84a40b78be9cd41f4925aacc
Daria <darrr2013@gmail.com>
2018-12-05 21:12:21 +0200

    paket makeover and litle fixes

--- src/main/java/com/danit/services/PaketService.java
+++ src/main/java/com/danit/services/PaketService.java
@@ -2,25 +2,8 @@ package com.danit.services;
 
 import com.danit.dto.service.PaketListRequestDto;
 import com.danit.models.Paket;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-public interface PaketService {
-
-  Page<Paket> getAllPakets(Pageable pageable);
-
-  Page<Paket> getAllPakets(PaketListRequestDto paketListRequestDto, Pageable pageable);
-
-  Paket getPaketById(long id);
-
-  List<Paket> savePakets(List<Paket> pakets);
-
-  List<Paket> updatePakets(List<Paket> pakets);
-
-  void deletePaketById(long id);
-
-  void deletePakets(List<Paket> pakets);
-
+@Service
+public class PaketService extends AbstractEntityService<Paket, PaketListRequestDto> {
 }
