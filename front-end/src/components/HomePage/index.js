12ea6240e07f56f404d75fee28a45dacdf6b2899
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-12-11 18:15:35 +0200

    deleting authorization wrapper and removing it's functional in homepage

--- front-end/src/components/HomePage/index.js
+++ front-end/src/components/HomePage/index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
-import withAuth from '../../helpers/withAuth';
 import './index.scss';
 import Header from '../Header';
 import AuthService from '../../helpers/authService';
@@ -9,30 +8,63 @@ import {faPlus, faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
 import UserNavigation from '../UserNavigation';
 import Admin from '../Admin';
 import Manager from '../Manager';
+import Settings from '../Settings';
+import {Loader} from 'semantic-ui-react';
 
 library.add(
   faPlus,
   faSignOutAlt
 );
 
-const auth = new AuthService();
+const auth = new AuthService(Settings.apiServerUrl);
 
 class HomePage extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+  }
+
   handleLogout = () => {
     auth.logout();
     this.props.history.replace('/login');
   };
 
   render () {
-    return (
-      <div className='home'>
-        <Header handleLogout={this.handleLogout} userName={this.props.user.sub}/>
-        <Route exact path='/' component={UserNavigation}/>
-        <Route path='/admin' component={Admin}/>
-        <Route path='/manager' component={Manager}/>
-      </div>
-    );
+    if (this.state.user) {
+      return (
+        <div className='home'>
+          <Header handleLogout={this.handleLogout} userName={this.state.user.sub}/>
+          <Route exact path='/' component={UserNavigation}/>
+          <Route path='/admin' component={Admin}/>
+          <Route path='/manager' component={Manager}/>
+        </div>
+      );
+    } else {
+      return (
+        <div className="loader-wrapper">
+          <Loader active inline='centered' size='massive'/>
+        </div>
+      );
+    }
+  }
+
+  componentDidMount () {
+    if (!auth.loggedIn()) {
+      this.props.history.replace('/login');
+    } else {
+      try {
+        const profile = auth.getProfile();
+        this.setState({
+          user: profile
+        });
+      } catch (err) {
+        auth.logout();
+        this.props.history.replace('/login');
+      }
+    }
   }
 }
 
-export default withAuth(HomePage);
+export default HomePage;
