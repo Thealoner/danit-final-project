88b816dbb0f6962215d0c64082012907904da6bd
Thealoner <the.aloner@gmail.com>
2018-12-19 18:21:43 +0200

    Refactoring: open new tab on menu click, moving storage to redux (#139)
    * open new tab on menu click, removed new tab button;
    
    * merged master
    
    * kinda stable
    
    * reducers in progress
    
    * Redux
    
    * Something is cracking
    
    * Changes redux folder / file names according to convention
    
    * package-lock
    
    * Fixed: closing last tab, chaning active tab when tab is closed
    
    * current tab, loading
    
    * current tab to mapStateToProps
    
    * Moved TabContent logic from TabContainer to TabContent
    
    * Moved TabContent logic from TabContainer to TabContent
    
    * Grid refactoring
    
    * Grid and Filter fixed
    
    * styles tabs
    
    * 'fixed' tabs
    
    * tabs.grid is still broken
    
    * some -> filter
    
    * Fixed tabs
    
    * styles for new tabs
    
    * Eslint
    
    * Stylelint

---
+++ front-end/src/filters/tabs.js
@@ -0,0 +1,16 @@
+import {
+  GET_CURRENT_TAB_KEY,
+  GET_TAB_FORM_DATA
+} from '../actions/types';
+
+export function getCurrentTabKey () {
+  return {
+    type: GET_CURRENT_TAB_KEY
+  };
+}
+
+export function getTabFormData (tabKey) {
+  return {
+    type: GET_TAB_FORM_DATA
+  };
+}
