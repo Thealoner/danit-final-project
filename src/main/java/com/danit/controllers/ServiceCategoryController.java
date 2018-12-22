993235074db2c437cde2a3804fcd2f0bbc75b6c2
Daria Lazurenko <darrr@users.noreply.github.com>
2018-12-22 11:16:50 +0200

    Service Category controller fix (#143)
    * service category changes
    
    * #service category jpql
    
    * service category changes
    
    * service category and service changes
    
    * some tests
    
    * more tests
    
    * forgot to change db

--- src/main/java/com/danit/controllers/ServiceCategoryController.java
+++ src/main/java/com/danit/controllers/ServiceCategoryController.java
@@ -3,9 +3,13 @@ package com.danit.controllers;
 import com.danit.dto.Views;
 import com.danit.dto.service.ServiceCategoryListRequestDto;
 import com.danit.facades.ServiceCategoryFacade;
+import com.danit.facades.ServiceFacade;
+import com.danit.models.Service;
 import com.danit.models.ServiceCategory;
+import com.danit.services.ServiceCategoryService;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -38,14 +42,22 @@ public class ServiceCategoryController {
 
   private static final String LOG_MSG_GOT_ALL_DATA = " got all service categories data";
   private ServiceCategoryFacade serviceCategoryFacade;
+  private ServiceCategoryService serviceCategoryService;
+  private ServiceFacade serviceFacade;
 
-  public ServiceCategoryController(ServiceCategoryFacade serviceCategoryFacade) {
+  @Autowired
+  public ServiceCategoryController(ServiceCategoryFacade serviceCategoryFacade,
+                                   ServiceCategoryService serviceCategoryService,
+                                   ServiceFacade serviceFacade) {
     this.serviceCategoryFacade = serviceCategoryFacade;
+    this.serviceCategoryService = serviceCategoryService;
+    this.serviceFacade = serviceFacade;
   }
 
   @JsonView(Views.Extended.class)
   @PostMapping
-  public ResponseEntity<Map<String, Object>> createServiceCategoriesDto(@RequestBody List<ServiceCategory> serviceCategories,
+  public ResponseEntity<Map<String, Object>> createServiceCategoriesDto(@RequestBody List<ServiceCategory>
+                                                                              serviceCategories,
                                                                         Principal principal) {
     log.info(principal.getName() + " is saving new service categories: " + serviceCategories);
     return ResponseEntity.ok(convertDtoToMap(serviceCategoryFacade.saveEntities(serviceCategories)));
@@ -114,29 +126,103 @@ public class ServiceCategoryController {
   @DeleteMapping
   void deleteServiceCategories(@RequestBody List<ServiceCategory> serviceCategories, Principal principal) {
     log.info(principal.getName() + " try to delete service category data: " + serviceCategories);
-    serviceCategoryFacade.deleteEntities(serviceCategories);
+    serviceCategoryService.deleteServiceCategory(serviceCategories);
   }
 
   @ResponseStatus(HttpStatus.OK)
   @DeleteMapping("/{id}")
   void deleteServiceCategoryById(@PathVariable(name = "id") long id, Principal principal) {
     log.info(principal.getName() + " try to delete service category with id: " + id);
-    serviceCategoryFacade.deleteEntityById(id);
+    serviceCategoryService.deleteServiceCategotryById(id);
   }
 
-//  @DeleteMapping("/{servCatId}/services/{serviceId}")
-//  void deleteServiceCategoryById(@PathVariable(name = "servCatId") long servCatId,
-//                                 @PathVariable(name = "serviceId") long serviceId,
-//                                 Principal principal) {
-//    logger.info(principal.getName() + " try to delete service with id: " + serviceId
-//        + " in service category with id: " + servCatId);
-//    serviceCategoryService.deleteServiceCategoryService(servCatId, serviceId);
-//  }
+  //------related entities methods--------------------------------------------------------------------------------------
 
-//  @GetMapping("/{id}/services")
-//  List<Service> getAllServiceCategoryServices(@PathVariable(name = "id") long id, Principal principal) {
-//    logger.info(principal.getName() + " got services from service category with id: " + id);
-//    return serviceCategoryService.getAllServiceCategoryServices(id);
-//  }
+  //Services
+
+  @JsonView(Views.Ids.class)
+  @GetMapping("{serviceCategoryId}/services/ids")
+  ResponseEntity<Map<String, Object>> getAllServiceCategoryServicesIds(@PathVariable(name = "serviceCategoryId")
+                                                                           long id,
+      @PageableDefault(page = DEFAULT_PAGE_NUMBER, size = DEFAULT_PAGE_SIZE)
+      @SortDefault.SortDefaults({
+          @SortDefault(sort = "id", direction = Sort.Direction.ASC)
+      }) Pageable pageable,
+      Principal principal) {
+    log.info(principal.getName() + " got service categories services data with id: " + id);
+    return ResponseEntity.ok(convertPageToMap(serviceFacade
+        .findAllServicesDtoForServiceCategoryId(id, pageable)));
+  }
+
+  @JsonView(Views.Short.class)
+  @GetMapping("{serviceCategoryId}/services/short")
+  ResponseEntity<Map<String, Object>> getAllServiceCategoryServicesShort(@PathVariable(name = "serviceCategoryId")
+                                                                             long id,
+                                      @PageableDefault(page = DEFAULT_PAGE_NUMBER, size = DEFAULT_PAGE_SIZE)
+                                      @SortDefault.SortDefaults({
+                                        @SortDefault(sort = "id", direction = Sort.Direction.ASC)
+                                        }) Pageable pageable,
+                                      Principal principal) {
+    log.info(principal.getName() + " got service categories services data with id: " + id);
+    return ResponseEntity.ok(convertPageToMap(serviceFacade
+        .findAllServicesDtoForServiceCategoryId(id, pageable)));
+  }
+
+  @JsonView(Views.Extended.class)
+  @GetMapping("{serviceCategoryId}/services")
+  ResponseEntity<Map<String, Object>> getAllServiceCategoryServicesExtended(@PathVariable(name = "serviceCategoryId")
+                                                                                long id,
+                                                                    @PageableDefault(page = DEFAULT_PAGE_NUMBER,
+                                                                        size = DEFAULT_PAGE_SIZE)
+                                                                    @SortDefault.SortDefaults({
+                                                                        @SortDefault(sort = "id",
+                                                                            direction = Sort.Direction.ASC)
+                                                                    }) Pageable pageable,
+                                                                    Principal principal) {
+    log.info(principal.getName() + " got service categories services data with id: " + id);
+    return ResponseEntity.ok(convertPageToMap(serviceFacade
+        .findAllServicesDtoForServiceCategoryId(id, pageable)));
+  }
+
+  @PutMapping("/{serviceCategoryId}/service/{serviceId}")
+  @ResponseStatus(HttpStatus.OK)
+  void assignServiceToServiceCategory(@PathVariable(name = "serviceCategoryId") Long serviceCategoryId,
+                              @PathVariable(name = "serviceId") Long serviceId,
+                              Principal principal) {
+    log.info(principal.getName() + " is trying to assign serviceId=" + serviceId +
+        " to serviceCategoryId = " + serviceCategoryId);
+    serviceCategoryService.assignServiceToServiceCategory(serviceCategoryId, serviceId);
+  }
+
+  @PutMapping("/{serviceCategoryId}/services")
+  @ResponseStatus(HttpStatus.OK)
+  void assignServicesToServiceCategory(@PathVariable(name = "serviceCategoryId") Long serviceCategoryId,
+                             @RequestBody List<Service> services,
+                             Principal principal) {
+    log.info(principal.getName() + " is trying to assign services" + services + " to serviceCategoryId = " +
+        serviceCategoryId);
+    serviceCategoryService.assignServicesToServiceCategory(serviceCategoryId, services);
+  }
+
+  @DeleteMapping("/{serviceCategoryId}/service/{serviceId}")
+  @ResponseStatus(HttpStatus.OK)
+  void deleteServiceFromServiceCategory(@PathVariable(name = "serviceCategoryId") Long serviceCategoryId,
+                                        @PathVariable(name = "serviceId") Long serviceId,
+                                Principal principal) {
+    log.info(principal.getName() + " is trying to delete serviceId=" + serviceId +
+        " from serviceCategoryId = " + serviceCategoryId);
+    serviceCategoryService.deleteServiceFromServiceCategory(serviceCategoryId, serviceId);
+  }
+
+  @DeleteMapping("/{serviceCategoryId}/services")
+  @ResponseStatus(HttpStatus.OK)
+  void deleteServicesFromServiceCategory(@PathVariable(name = "serviceCategoryId") Long serviceCategoryId,
+                                         @RequestBody List<Service> services,
+                                         Principal principal) {
+    log.info(principal.getName() + " is trying to assign services" + services + " to serviceCategoryId = " +
+        serviceCategoryId);
+    serviceCategoryService.deleteServicesFromServiceCategory(serviceCategoryId, services);
+  }
+
 
 }
--- src/main/java/com/danit/facades/ServiceCategoryFacade.java
+++ src/main/java/com/danit/facades/ServiceCategoryFacade.java
@@ -3,9 +3,24 @@ package com.danit.facades;
 import com.danit.dto.ServiceCategoryDto;
 import com.danit.dto.service.ServiceCategoryListRequestDto;
 import com.danit.models.ServiceCategory;
+import com.danit.services.ServiceCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServiceCategoryFacade extends AbstractDtoFacade<ServiceCategoryDto, ServiceCategory,
     ServiceCategoryListRequestDto> {
+
+  private ServiceCategoryService serviceCategoryService;
+
+  @Autowired
+  public ServiceCategoryFacade(ServiceCategoryService serviceCategoryService) {
+    this.serviceCategoryService = serviceCategoryService;
+  }
+
+  public Page<ServiceCategoryDto> findAllServiceCategoriesOfServiceWithId(long id, Pageable pageable) {
+    return convertToDtos(serviceCategoryService.findAllServiceCategoriesOfServiceWithId(id, pageable));
+  }
 }
