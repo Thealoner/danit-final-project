96ecbc0ab225410b22577fb2c56f4c2720441e76
garmashs@gmail.com <20963520aA>
2018-12-23 15:04:26 +0200

    #auth endpoint moved to /auth

--- src/main/java/com/danit/security/WebSecurityConfig.java
+++ src/main/java/com/danit/security/WebSecurityConfig.java
@@ -1,12 +1,12 @@
 package com.danit.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,8 +23,6 @@ import java.util.Arrays;
 
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-@ComponentScan("com.danit.security")
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -36,19 +34,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager());
+    jwtAuthenticationFilter.setFilterProcessesUrl("/auth");
     http
         .cors()
         .and()
         .csrf().disable()
         .authorizeRequests()
-        .antMatchers("/log").permitAll()
         .antMatchers("/socket/**").permitAll()
         .antMatchers(HttpMethod.GET, "/users/**").hasAuthority("ADMIN")
         .anyRequest().authenticated()
         .and()
         .addFilterBefore(new JwtAuthorizationFilter(authenticationManager(), userDetailsService),
             BasicAuthenticationFilter.class)
-        .addFilterBefore(new JwtAuthenticationFilter(authenticationManager()),
+        .addFilterBefore(jwtAuthenticationFilter,
             UsernamePasswordAuthenticationFilter.class)
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
