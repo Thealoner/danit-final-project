99e61835f4c17576ad67c288dba778d855d7fac2
Eduard Nikolaiev <the.aloner@gmail.com>
2018-12-24 00:07:59 +0200

    ununsed imports

--- src/main/java/com/danit/security/WebSecurityConfig.java
+++ src/main/java/com/danit/security/WebSecurityConfig.java
@@ -1,10 +1,8 @@
 package com.danit.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
