c77279c131feef4fca68008fb674e005b42cc738
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-12-24 21:26:38 +0200

    disable filter button depending on 'filtered' state's property

--- front-end/src/components/Admin/Grid/Filter/index.js
+++ front-end/src/components/Admin/Grid/Filter/index.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './index.scss';
 
 let defaultFilter = {
   field: 'search',
   value: ''
 };
+
 class Filter extends Component {
   state = {
-    ...defaultFilter
+    ...defaultFilter,
+    filtered: false
   };
-
-  apply_filter = React.createRef();
 
   handleInputChange = event => {
     const target = event.target;
@@ -18,7 +18,8 @@ class Filter extends Component {
     const name = target.name;
 
     this.setState({
-      [name]: value
+      [name]: value,
+      filtered: false
     });
   };
 
@@ -26,47 +27,50 @@ class Filter extends Component {
     event.preventDefault();
     const filterString = '&' + this.state.field + '=' + this.state.value;
     this.props.applyFilter(filterString);
+    this.setState({
+      filtered: true
+    })
   };
 
   clearFilter = () => {
     this.props.clearFilter();
-    this.apply_filter.classList.remove('filter__apply-btn-active');
     this.setState({
-      ...defaultFilter
+      ...defaultFilter,
+      filtered: false
     });
   };
 
   renderFields = () => {
-    const { columns } = this.props;
-    
+    const {columns} = this.props;
+
     let fields = columns.map(column => (
       <option key={column.field} value={column.field}>{column.title}</option>
     ));
 
-    fields.unshift(<option key="search" value="search">Все</option>);
-      
+    fields.unshift(<option key='search' value='search'>Все</option>);
+
     return fields;
   };
 
-  applyFilter = () => this.apply_filter.classList.add('filter__apply-btn-active');
-
-  render () {
+  render() {
     return (
-      <form onSubmit={this.onSubmit} className="filter" >
+      <form onSubmit={this.onSubmit} className='filter'>
         <span>
           <label>Поле: </label>
-          <select name="field" value={this.state.field} onChange={this.handleInputChange}>
+          <select name='field' value={this.state.field} onChange={this.handleInputChange}>
             {this.renderFields()}
           </select>
         </span>
-
         <span>
           <label>Значение: </label>
-          <input name="value" type="text" placeholder="" value={this.state.value} onChange={this.handleInputChange} />
+          <input name='value' type='text' placeholder='' value={this.state.value} onChange={this.handleInputChange}/>
         </span>
-
-        <button name="filter" type="submit" className="filter__apply-btn" ref={el => this.apply_filter = el} onClick={() => this.applyFilter()}>Применить фильтр</button>
-        <button name="clear" className="filter__clear-btn" onClick={this.clearFilter} type="button">Очистить фильтр</button>
+        <button name='filter' type='submit' disabled={this.state.filtered}>
+          Применить фильтр
+        </button>
+        <button name='clear' onClick={this.clearFilter} type='button'>
+          Очистить фильтр
+        </button>
       </form>
     );
   }
