b2f96358fd80501cabcc6f9e0f4373420e4bf63f
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-12-25 22:23:45 +0200

    logo in empty tab

--- front-end/src/components/Admin/TabContainer/TabPane/index.js
+++ front-end/src/components/Admin/TabContainer/TabPane/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import Tab from './Tab';
 
-function TabPane ({ tabs, onSelect }) {
-  const tabsList = tabs.tabsArray.map(
-    tab =>
-      <Tab
-        key={tab.tabKey}
-        tabKey={tab.tabKey}
-        title={tab.title}
-        activeKey={tabs.activeKey}
-        onSelect={onSelect}
-        edited={tab.form && tab.form.edited}
-      />
+function TabPane({tabs, onSelect}) {
+  const tabsList = tabs.tabsArray.map(tab =>
+    <Tab
+      key={tab.tabKey}
+      tabKey={tab.tabKey}
+      title={tab.title}
+      activeKey={tabs.activeKey}
+      onSelect={onSelect}
+      edited={tab.form && tab.form.edited}
+    />
   );
-  
+
   return <ul className='tabs__pane'><span className='tabs__pane-skirt'>{tabsList}</span></ul>;
 }
 
