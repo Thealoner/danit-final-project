4051091ca1fa4310f24b8964b99ff28f0de3e7f3
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-12-25 16:07:13 +0200

    double quotes attributes

--- front-end/src/components/Admin/Grid/Filter/index.js
+++ front-end/src/components/Admin/Grid/Filter/index.js
@@ -47,30 +47,26 @@ class Filter extends Component {
       <option key={column.field} value={column.field}>{column.title}</option>
     ));
 
-    fields.unshift(<option key='search' value='search'>Все</option>);
+    fields.unshift(<option key="search" value="search">Все</option>);
 
     return fields;
   };
 
   render () {
     return (
-      <form onSubmit={this.onSubmit} className='filter'>
+      <form onSubmit={this.onSubmit} className="filter">
         <span>
           <label>Поле: </label>
-          <select name='field' value={this.state.field} onChange={this.handleInputChange}>
+          <select name="field" value={this.state.field} onChange={this.handleInputChange}>
             {this.renderFields()}
           </select>
         </span>
         <span>
           <label>Значение: </label>
-          <input name='value' type='text' placeholder='' value={this.state.value} onChange={this.handleInputChange}/>
+          <input name="value" type="text" value={this.state.value} onChange={this.handleInputChange}/>
         </span>
-        <button name='filter' type='submit' disabled={this.state.filtered}>
-          Применить фильтр
-        </button>
-        <button name='clear' onClick={this.clearFilter} type='button'>
-          Очистить фильтр
-        </button>
+        <button name="filter" type="submit" disabled={this.state.filtered}>Применить фильтр</button>
+        <button name="clear" onClick={this.clearFilter} type="button">Очистить фильтр</button>
       </form>
     );
   }
