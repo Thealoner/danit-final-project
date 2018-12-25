d40e161256263dc1a72655e2cb2299a8d800f4e3
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-12-26 00:07:04 +0200

    hover titles for tabs

--- front-end/src/components/Header/User/index.js
+++ front-end/src/components/Header/User/index.js
@@ -5,13 +5,13 @@ import photo from './user-avatar.png';
 
 const User = ({handleLogout, userName}) =>
   <div className="user">
-    <img src={photo} className="user__avatar" title={userName} alt="user-avatar"/>
+    <img src={photo} className="user__avatar" alt="user-avatar"/>
     <div className="user__info">
       <p className="user__name"> {userName}</p>
       <p className="user__position"> Менеджер продаж</p>
     </div>
     <button className="user__btn-logout" onClick={handleLogout}>
-      <FontAwesomeIcon icon="sign-out-alt" size="1x"/>
+      <FontAwesomeIcon icon="sign-out-alt" size="1x" title="Выйти"/>
     </button>
   </div>;
 
