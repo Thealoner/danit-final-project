1ef7e1f7f0d135f86ad7258962e37e6fda0e07ec
Thealoner <the.aloner@gmail.com>
2018-12-26 13:10:51 +0200

    Stomp front

--- front-end/src/components/Stomp/index.js
+++ front-end/src/components/Stomp/index.js
@@ -3,6 +3,9 @@ import SockJsClient from 'react-stomp';
 import AuthService from '../../helpers/authService';
 
 class Stomp extends Component {
+  state = {
+    loaded: false
+  }
   sendMessage = (msg) => {
     this.clientRef.sendMessage('/topics/all', msg);
   }
@@ -13,16 +16,27 @@ class Stomp extends Component {
     const headers = {
       'Authorization': 'Bearer ' + this.authService.getToken()
     };
+
+    if (this.state.loaded) {
+      return <div></div>;
+    }
+    
     return (
       <div>
         <SockJsClient
           url='http://localhost:9000/socket'
-          topics={'/events/put'}
+          topics={['/events/put']}
           headers={headers}
           onMessage={(msg) => { console.log(msg); }}
           ref={(client) => { this.clientRef = client; }} />
       </div>
     );
+  }
+
+  componentDidMount () {
+    this.setState({
+      loaded: true
+    });
   }
 }
 
