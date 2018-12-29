48806050b7ef7f8bb1109ba2f307b0d48b62f42d
Artem <art0938009994@gmail.com>
2018-12-29 16:54:45 +0200

    Adding "cancel" button in form editor

--- front-end/src/components/Admin/Record/RecordEditor/index.js
+++ front-end/src/components/Admin/Record/RecordEditor/index.js
@@ -94,6 +94,7 @@ class RecordEditor extends Component {
           onSubmit={mode === 'edit' ? this.putData : this.postData}
           onError={error => toastr.error('Пожалуйста, проверьте введеные данные', error)}>
           <button disabled={this.state.loading} type='submit' className='record__button'>Сохранить</button>
+          <button disabled={this.state.loading} type='submit' className='record__button'>Отмена</button>
         </Form>
       </Fragment>
     );
