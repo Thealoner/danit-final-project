28bd04899ebf25301a0cf5f0448a15932629208b
Thealoner <the.aloner@gmail.com>
2018-12-29 12:04:39 +0200

    Sockets front (#154)
    * Stomp front
    
    * #fixed 500 - internal server error with empty search parameters
    #working native stomp react example
    
    * #entity service makeover
    
    * #front end style
    
    * #front end style

--- front-end/src/components/Stomp/index.js
+++ front-end/src/components/Stomp/index.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react';
-import SockJsClient from 'react-stomp';
+import Stomp from 'stompjs';
 import AuthService from '../../helpers/authService';
 
-class Stomp extends Component {
-  sendMessage = (msg) => {
-    this.clientRef.sendMessage('/topics/all', msg);
+class SocketComponent extends Component {
+  state = {
+    loaded: false
   }
 
   authService = new AuthService();
 
   render () {
-    const headers = {
-      'Authorization': 'Bearer ' + this.authService.getToken()
-    };
+    if (this.state.loaded) {
+      return <div></div>;
+    }
+
     return (
-      <div>
-        <SockJsClient
-          url='http://localhost:9000/socket'
-          topics={'/events/put'}
-          headers={headers}
-          onMessage={(msg) => { console.log(msg); }}
-          ref={(client) => { this.clientRef = client; }} />
-      </div>
+      <div></div>
     );
-  }
+  };
+
+  componentDidMount () {
+    this.setState({
+      loaded: true
+    });
+
+    const headers = {
+      'Authorization': this.authService.getToken()
+    };
+
+    let client = Stomp.client('ws://localhost:9000/socket');
+    client.connect(headers, (frame) => {
+      client.subscribe('/events/put', (frame) => {
+      }, headers);
+    });
+  };
 }
 
-export default Stomp;
+export default SocketComponent;
