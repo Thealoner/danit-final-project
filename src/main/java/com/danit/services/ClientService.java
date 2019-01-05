db193fe5813f23e8052ce403d3733be26c61d154
Serhii Harmash <garmashs@gmail.com>
2019-01-05 13:09:45 +0200

    #clients deleting implemented

--- src/main/java/com/danit/services/ClientService.java
+++ src/main/java/com/danit/services/ClientService.java
@@ -2,17 +2,43 @@ package com.danit.services;
 
 import com.danit.dto.service.ClientListRequestDto;
 import com.danit.models.Client;
+import com.danit.models.Contract;
 import com.danit.repositories.ClientRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 @Service
 public class ClientService extends AbstractBaseEntityService<Client, ClientListRequestDto> {
 
   private ClientRepository clientRepository;
-  private PaketService paketService;
 
-  public ClientService(ClientRepository clientRepository, PaketService paketService) {
+  public ClientService(ClientRepository clientRepository) {
     this.clientRepository = clientRepository;
-    this.paketService = paketService;
+  }
+
+  @Override
+  @Transactional
+  public void deleteEntityById(long id) {
+    Client client = getEntityById(id);
+    if(Objects.nonNull(client.getContracts())) {
+      client.getContracts().forEach(contract -> contract.setClient(null));
+    }
+    clientRepository.deleteById(id);
+  }
+
+  @Override
+  @Transactional
+  public void deleteEntities(List<Client> entityList) {
+    List<Client> clients = reloadEntities(entityList);
+    clients.forEach(client -> {
+      if(Objects.nonNull(client.getContracts())) {
+        client.getContracts().forEach(contract -> contract.setClient(null));
+      }
+    });
+    clientRepository.deleteAll(clients);
   }
 }
