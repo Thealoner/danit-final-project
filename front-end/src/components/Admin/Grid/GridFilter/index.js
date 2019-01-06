0b5b05ad0f9c3122af861a732bb33cd1e687dfb6
Yaroslav Nikiforov <mbuu8888@gmail.com>
2019-01-06 18:16:37 +0200

    eslint fix

--- front-end/src/components/Admin/Grid/GridFilter/index.js
+++ front-end/src/components/Admin/Grid/GridFilter/index.js
@@ -56,21 +56,21 @@ class GridFilter extends Component {
     return (
       <form onSubmit={this.onSubmit} className="filter">
         <div>
-        <span>
-          <label>Поле: </label>
-          <select name="field" value={this.state.field} onChange={this.handleInputChange}>
-            {this.renderFields()}
-          </select>
-        </span>
-        <span>
-          <label>Значение: </label>
-          <input name="value" type="text" value={this.state.value} onChange={this.handleInputChange}/>
-        </span>
+          <span>
+            <label>Поле: </label>
+            <select name="field" value={this.state.field} onChange={this.handleInputChange}>
+              {this.renderFields()}
+            </select>
+          </span>
+          <span>
+            <label>Значение: </label>
+            <input name="value" type="text" value={this.state.value} onChange={this.handleInputChange}/>
+          </span>
         </div>
-          <div>
-        <button name="filter" type="submit" disabled={this.state.filtered}>Применить фильтр</button>
-        <button name="clear" onClick={this.clearFilter} type="button">Очистить фильтр</button>
-         </div>
+        <div>
+          <button name="filter" type="submit" disabled={this.state.filtered}>Применить фильтр</button>
+          <button name="clear" onClick={this.clearFilter} type="button">Очистить фильтр</button>
+        </div>
       </form>
     );
   }
