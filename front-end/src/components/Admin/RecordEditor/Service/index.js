57c3207fef21d6d74b9aa96c3e35b4fcb8e2e5a3
Eduard Nikolaiev <the.aloner@gmail.com>
2019-01-09 21:44:20 +0200

    Initial setup

---
+++ front-end/src/components/Admin/RecordEditor/Service/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Service extends Component {
+  render () {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+export default Service;
--- front-end/src/components/Admin/TabContainer/TabContent/index.js
+++ front-end/src/components/Admin/TabContainer/TabContent/index.js
@@ -2,28 +2,30 @@ import React, { Component } from 'react';
 import Grid from '../../Grid';
 import { Loader } from 'semantic-ui-react';
 import RecordEditor from '../../RecordEditor';
+import Service from '../../RecordEditor/Service';
 
 class TabContent extends Component {
   render () {
     const { currentTab } = this.props;
+    let content;
 
     if (currentTab.tabStatus === 'loading') {
-      return (
-        <div className="tabs__content">
-          <div className="tabs__loader-wrapper"><Loader active inline='centered' size='big'/></div>
-        </div>
-      );
-    } else {
-      return (
-        currentTab.type === 'grid'
-          ? <div className="tabs__content">
-            <Grid currentTab={currentTab} />
-          </div>
-          : <div className="tabs__content">
-            <RecordEditor currentTab={currentTab} />
-          </div>
-      );
+      content = <div className="tabs__loader-wrapper"><Loader active inline='centered' size='big'/></div>;
+    } else if (currentTab.type === 'grid') {
+      content = <Grid currentTab={currentTab} />;
+    } else if (currentTab.type === 'form') {
+      if (currentTab.tabKey === 'services') {
+        content = <Service currentTab={currentTab} />;
+      } else {
+        content = <RecordEditor currentTab={currentTab} />;
+      }
     }
+
+    return (
+      <div className="tabs__content">
+        {content}
+      </div>
+    );
   }
 }
 
