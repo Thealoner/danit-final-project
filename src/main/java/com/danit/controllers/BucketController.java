7bdb97fe4dc098ab2ea6652686d2fc0a45d25a3f
Serhii Harmash <garmashs@gmail.com>
2019-01-19 11:17:17 +0200

    #bucket controller getImage method impl

--- src/main/java/com/danit/controllers/BucketController.java
+++ src/main/java/com/danit/controllers/BucketController.java
@@ -1,13 +1,23 @@
 package com.danit.controllers;
 
+import com.amazonaws.util.IOUtils;
 import com.danit.services.amazon.AmazonClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/storage/")
@@ -21,12 +31,21 @@ public class BucketController {
   }
 
   @PostMapping("/uploadFile")
-  public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
-    return amazonClientService.uploadFile(file);
+  public void uploadFile(@RequestPart(value = "file") MultipartFile file) {
+    amazonClientService.uploadFile(file);
   }
 
   @DeleteMapping("/deleteFile")
-  public String deleteFile(@RequestPart(value = "url") String fileUrl) {
-    return amazonClientService.deleteFileFromS3Bucket(fileUrl);
+  public void deleteFile(@RequestPart(value = "url") String fileUrl) {
+   amazonClientService.deleteFileFromS3Bucket(fileUrl);
+  }
+
+  @GetMapping("/{fileName}")
+  public ResponseEntity<byte[]> getImageAsResponseEntity(@PathVariable(name = "fileName") String fileName) throws IOException {
+    HttpHeaders headers = new HttpHeaders();
+    InputStream in = amazonClientService.getFile(fileName);
+    byte[] media = IOUtils.toByteArray(in);
+    headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+    return new ResponseEntity<>(media, headers, HttpStatus.OK);
   }
 }
