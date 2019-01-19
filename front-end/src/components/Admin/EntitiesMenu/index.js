6c57d122c498f2ad71cf99834de4dd0c121d0914
Yaroslav Nikiforov <mbuu8888@gmail.com>
2019-01-19 16:49:49 +0200

    active links switching

--- front-end/src/components/Admin/EntitiesMenu/index.js
+++ front-end/src/components/Admin/EntitiesMenu/index.js
@@ -3,14 +3,14 @@ import gridEntities from '../gridEntities';
 import { connect } from 'react-redux';
 import { openTab, getGridData, loadingTab } from '../../../actions/tabActions';
 
-function EntitiesMenu ({openTab, loadingTab, getGridData}) {
+function EntitiesMenu ({ openTab, loadingTab, getGridData, currentTab }) {
   const links = [];
 
   gridEntities.forEach((entity) => {
     links.push(
       <span
         key={entity.id}
-        className="configurator__link"
+        className={`configurator__link${currentTab && entity.id === currentTab.tabKey ? ' configurator__link--active' : ''}`}
         onClick={() => {
           openTab(entity.id, {type: 'grid'});
           loadingTab();
@@ -26,8 +26,14 @@ function EntitiesMenu ({openTab, loadingTab, getGridData}) {
 }
 
 const mapStateToProps = state => {
+  let currentTab = null;
+
+  if (state.tabs.activeKey && state.tabs.tabsArray.length > 0) {
+    currentTab = state.tabs.tabsArray.filter(t => t.tabKey === state.tabs.activeKey)[0];
+  }
+
   return {
-    tabs: state.tabs
+    currentTab
   };
 };
 
