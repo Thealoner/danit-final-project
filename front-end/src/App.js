2ab677d8d140f3671ba1c8ba455dbc9baa213cff
olegmikadze <olegmikadze10@gmail.com>
2019-01-20 17:52:52 +0200

    forgot_password

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
+import ResetPassword from './components/ResetPassword';
 
 const App = () =>
   <Switch>
     <Route path="/login" component={LoginPage}/>
     <Route path="/" component={HomePage}/>
+    <Route path="/login/reset" component={ResetPassword}/>
   </Switch>;
 
 export default App;
