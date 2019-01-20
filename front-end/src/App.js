6cb47c6f31efc7b39a40a40cd5a509c807e1fbe1
olegmikadze <olegmikadze10@gmail.com>
2019-01-20 18:17:53 +0200

    forgot_pass fixed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,11 +4,21 @@ import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 import ResetPassword from './components/ResetPassword';
 
-const App = () =>
+// import AppliedRoute from './PasswordComponents/AppliedRoute';
+// import AuthenticatedRoute from './PasswordComponents/AuthenticatedRoute';
+import UnauthenticatedRoute from './PasswordComponents/UnauthenticatedRoute';
+
+const App = (childProps) =>
   <Switch>
+    <Route path="/login/reset" component={ResetPassword}/>
+    <UnauthenticatedRoute
+      path="/login/reset"
+      exact
+      component={ResetPassword}
+      props={childProps}
+    />
     <Route path="/login" component={LoginPage}/>
     <Route path="/" component={HomePage}/>
-    <Route path="/login/reset" component={ResetPassword}/>
   </Switch>;
 
 export default App;
