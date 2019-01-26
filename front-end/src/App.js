be2ba08a5d36116fc50ca7b134d412f55541b546
Thealoner <the.aloner@gmail.com>
2019-01-26 10:41:16 +0200

    Reset password (#188)
    * #maven spring boot plugin
    
    * #password reset methods implemented
    
    * forgot_password
    
    * #password change method impl
    #changed endpoints auth for user change/update/reset password
    
    * forgot_pass fixed
    
    * eslint styles fixed
    
    * eslint styles fixed
    
    * #password change tests
    
    * #password change tests
    
    * #small changes in code
    
    * fixed errors forgot pass
    
    * Merge branch 'master' into reset_password
    
    # Conflicts:
    #	front-end/package-lock.json
    #	front-end/package.json
    
    * #Auth service added methods for password reset/change
    
    * #Auth service added methods for password reset/change
    
    * #code style
    
    * Added email field for users to the gird and editor
    
    * Stylelint
    
    * Forgotten change

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,10 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 import ResetPassword from './components/ResetPassword';
-
-// import AppliedRoute from './PasswordComponents/AppliedRoute';
-// import AuthenticatedRoute from './PasswordComponents/AuthenticatedRoute';
-import UnauthenticatedRoute from './PasswordComponents/UnauthenticatedRoute';
+import UnauthenticatedRoute from './components/LoginPage/UnauthenticatedRoute';
 
 const App = (childProps) =>
   <Switch>
