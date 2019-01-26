5bd5c5c65bb7a8c850f7dc7cc7b9209f07cec3c1
Serhii Harmash <garmashs@gmail.com>
2019-01-26 21:35:55 +0200

    #changed header - it shows actual user photo stored in s3 bucket

--- front-end/src/components/Header/User/index.js
+++ front-end/src/components/Header/User/index.js
@@ -1,24 +1,45 @@
-import React, { Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
 import { NavLink } from 'react-router-dom';
 import './index.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import photo from './user-avatar.png';
+import ajaxRequest from "../../../helpers/ajaxRequest";
+import {toastr} from "react-redux-toastr";
 
-const User = ({handleLogout, userName}) =>
+class User extends Component {
+  state = {
+    binaryData: ''
+  };
 
-  <Fragment>
-    <NavLink to="/profile">
-      <div className="user">
-        <img src={photo} className="user__avatar" alt="user-avatar"/>
-        <div className="user__info">
-          <p className="user__name"> {userName}</p>
-          <p className="user__position"> Менеджер продаж</p>
-        </div>
-      </div>
-    </NavLink>
-    <button className="user__btn-logout" onClick={handleLogout}>
-      <FontAwesomeIcon icon="sign-out-alt" size="1x" title="Выйти"/>
-    </button>
-  </Fragment>;
+  componentDidMount () {
+    ajaxRequest.get('api/storage/avatar', 'storage')
+      .then(data => {
+        this.setState({
+          binaryData: data
+        });
+      })
+      .catch(() => {
+        toastr.error('Cant get avatar image');
+      });
+  }
+
+  render () {
+    return (
+      <Fragment>
+        <NavLink to="/profile">
+          <div className="user">
+            <img src={`data:image/png;base64,${this.state.binaryData}`} className="user__avatar" alt="user-avatar"/>
+            <div className="user__info">
+              <p className="user__name"> {this.props.userName}</p>
+              <p className="user__position"> Менеджер продаж</p>
+            </div>
+          </div>
+        </NavLink>
+        <button className="user__btn-logout" onClick={this.props.handleLogout}>
+          <FontAwesomeIcon icon="sign-out-alt" size="1x" title="Выйти"/>
+        </button>
+      </Fragment>
+    );
+  };
+}
 
 export default User;
