68e3734aadadf403cd6253f821f0af96f7ddfbb2
Oleg <olegmikadze10@gmail.com>
2019-01-26 10:39:33 +0200

    Reset password  (#185)
    * #maven spring boot plugin
    
    * #password reset methods implemented
    
    * forgot_password
    
    * #password change method impl
    #changed endpoints auth for user change/update/reset password
    
    * forgot_pass fixed
    
    * eslint styles fixed
    
    * eslint styles fixed
    
    * #password change tests
    
    * #password change tests
    
    * #small changes in code
    
    * fixed errors forgot pass
    
    * Merge branch 'master' into reset_password
    
    # Conflicts:
    #	front-end/package-lock.json
    #	front-end/package.json
    
    * #Auth service added methods for password reset/change
    
    * #Auth service added methods for password reset/change
    
    * #code style
    
    * Added email field for users to the gird and editor
    
    * Stylelint

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,9 +2,21 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
+import ResetPassword from './components/ResetPassword';
 
-const App = () =>
+// import AppliedRoute from './PasswordComponents/AppliedRoute';
+// import AuthenticatedRoute from './PasswordComponents/AuthenticatedRoute';
+import UnauthenticatedRoute from './PasswordComponents/UnauthenticatedRoute';
+
+const App = (childProps) =>
   <Switch>
+    <Route path="/login/reset" component={ResetPassword}/>
+    <UnauthenticatedRoute
+      path="/login/reset"
+      exact
+      component={ResetPassword}
+      props={childProps}
+    />
     <Route path="/login" component={LoginPage}/>
     <Route path="/" component={HomePage}/>
   </Switch>;
