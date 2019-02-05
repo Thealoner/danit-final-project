c80a15b795ad31036c627bf4ccd6f19095713580
Thealoner <the.aloner@gmail.com>
2019-02-05 22:54:54 +0200

    Initialize users entity with roles received from server (#205)

--- front-end/src/components/Admin/EntityEditor/User/index.js
+++ front-end/src/components/Admin/EntityEditor/User/index.js
@@ -9,36 +9,54 @@ import RenderField from '../Fields/RenderField';
 import RenderMultiSelect from '../Fields/RenderMultiSelect';
 import validateAllRequired from '../../../../helpers/validateAllRequired';
 import warningTest from '../../../../helpers/warningTest';
-
-const rolesOptions = [
-  {
-    key: 'ADMIN',
-    text: 'ADMIN',
-    value: '1'
-  },
-  {
-    key: 'USER',
-    text: 'USER',
-    value: '2'
-  }
-];
+import ajaxRequest from '../../../../helpers/ajaxRequest';
+import { toastr } from 'react-redux-toastr';
+import { Loader } from 'semantic-ui-react';
 
 class User extends Component {
+  state = {
+    rolesOptions: [],
+    rolesLoaded: false
+  }
+  
   changeField = (e, data, fieldName) => {
     this.props.change(fieldName, data.value);
   };
+
+  initRoles = () => {
+    ajaxRequest.get('/roles')
+      .then(response => {
+        const rolesOptions = response.data.map(role => ({
+          key: role.role,
+          text: role.role,
+          value: '' + role.id
+        }));
+        
+        this.setState({
+          rolesOptions: rolesOptions,
+          rolesLoaded: true
+        });
+      },
+      error => {
+        toastr.error(error.message);
+      });
+  }
 
   render () {
     const { currentTab, handleDelete, handleCancel, handleSubmit, submitting } = this.props;
     const { data } = currentTab.form;
     const editMode = !!data.id;
 
+    if (!this.state.rolesLoaded) {
+      return <div className="tabs__loader-wrapper"><Loader active inline='centered' size='big'/></div>;
+    }
+
     return (
       <form onSubmit={handleSubmit} className="record">
         <div className="form-group field field-object">
           <p>{editMode ? 'ID: ' + data.id : ''}</p>
           <Field name="username" component={RenderField} type="text" label="Логин" />
-          <Field name="roles" component={RenderMultiSelect} type="text" label="Роль" options={rolesOptions} changeField={(e, data) => this.changeField(e, data, 'roles')} />
+          <Field name="roles" component={RenderMultiSelect} type="text" label="Роль" options={this.state.rolesOptions} changeField={(e, data) => this.changeField(e, data, 'roles')} />
           <Field name="email" component={RenderField} type="text" label="Email" />
 
           <button type="submit" className="record__button" disabled={!currentTab.form.edited || submitting}>Сохранить</button>
@@ -50,14 +68,16 @@ class User extends Component {
   }
 
   componentDidMount () {
-    const { currentTab, initialize } = this.props;
-    const { data } = currentTab.form;
-    
-    initialize(data);
+    this.initRoles();
   }
 
-  componentDidUpdate () {
-    const { dirty, handleChange, formValues } = this.props;
+  componentDidUpdate (prevProps, prevState) {
+    const { currentTab, initialize, dirty, handleChange, formValues } = this.props;
+    const { data } = currentTab.form;
+
+    if (!prevState.rolesLoaded && this.state.rolesLoaded) {
+      initialize(data);
+    }
     
     if (dirty) {
       handleChange({
