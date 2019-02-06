a607251177f05e71b95e5dbd102dedffee76dc99
Daria <darrr2013@gmail.com>
2019-02-06 23:50:43 +0200

    m

--- src/main/java/com/danit/controllers/UserRoleController.java
+++ src/main/java/com/danit/controllers/UserRoleController.java
@@ -1,7 +1,6 @@
 package com.danit.controllers;
 
 
-import com.danit.dto.UserRoleDto;
 import com.danit.dto.Views;
 import com.danit.dto.service.UserRoleListRequestDto;
 import com.danit.facades.UserFacade;
@@ -18,7 +17,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
